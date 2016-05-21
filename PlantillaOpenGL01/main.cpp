7e956190c8cdd49d355f5046df6fd4e900f933d2
josema19 <josemacloud@gmail.com>
2016-05-21 18:18:23 -0400

    Modificada Función de Animación y Función de Teclado. Agregadas variables gobales y comentado un poco el código.

--- PlantillaOpenGL01/main.cpp
+++ PlantillaOpenGL01/main.cpp
@@ -10,7 +10,6 @@ using namespace std;
 #define DEF_floorGridXSteps	10.0
 #define DEF_floorGridZSteps	10.0
 
-
 GLUnurbsObj *theNurb;
 
 GLfloat ctlpoints[21][21][3] = {{{0}}};
@@ -29,23 +28,37 @@ typedef struct{
 }Vector2D;
 
 
-// Valores para la ola 1
-float L1	= 0.0f;				//Distancia de la ola 1
-float A1	= 0.0f;				//Altura de la ola 1
-float S1	= 0.0f;				//Velocidad de la ola 1
-Vector2D D1	= {0};				//Vector que determina la dirección de la ola 1
+// Valores de entrada para la ola 1
+float L1	= 8.0f;				//Distancia de la ola 1
+float A1	= 0.4f;				//Altura de la ola 1
+float S1	= 2.0f;				//Velocidad de la ola 1
+Vector2D D1	= {0.0,-1.0};		//Vector que determina la dirección de la ola 1
 
-// Valores para la ola 2
-float L2    = 0.0f;				//Distancia de la ola 2
-float A2    = 0.0f;				//Altura de la ola 2
-float S2	   = 0.0f;			//Velocidad de la ola 2
-Vector2D D2 = {0};				//Vector que determina la dirección de la ola 2
+//Variables para la ola 1
+float w1;
+float signo1;
+float escalar1;
+Vector2D normal1;
 
-float t = 0.0f;
+// Valores de entrada para la ola 2
+float L2    = 4.0f;				//Distancia de la ola 2
+float A2    = 0.3f;				//Altura de la ola 2
+float S2	= 1.0f;				//Velocidad de la ola 2
+Vector2D D2 = {1.0,1.0};		//Vector que determina la dirección de la ola 2
 
+//Variables para la ola 2
+float w2;
+float signo2;
+float escalar2;
+Vector2D normal2;
+
+float t			 = 0.0f;
+float pi		 = 3.141592f;
+bool  pausar	 = true;
+bool activarOla1 = true;
+bool activarOla2 = true;
 
 void ejesCoordenada() {
-	
 	glLineWidth(2.5);
 	glBegin(GL_LINES);
 		glColor3f(1.0,0.0,0.0);
@@ -60,65 +73,53 @@ void ejesCoordenada() {
 	int i;
 	glColor3f(0.0,1.0,0.0);
 	glBegin(GL_LINES);
-		for(i = -10; i <=10; i++){
+		for(i = -10; i <=10; i++) {
 			if (i!=0) {		
 				if ((i%2)==0){	
 					glVertex2f(i,0.4);
 					glVertex2f(i,-0.4);
-
 					glVertex2f(0.4,i);
 					glVertex2f(-0.4,i);
-				}else{
+				}else {
 					glVertex2f(i,0.2);
 					glVertex2f(i,-0.2);
-
 					glVertex2f(0.2,i);
 					glVertex2f(-0.2,i);
-
 				}
 			}
 		}
-		
 	glEnd();
-
 	glLineWidth(1.0);
 }
 
 void changeViewport(int w, int h) {
-	
 	float aspectratio;
-
-	if (h==0)
-		h=1;
-
-	
-   glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
-   glMatrixMode (GL_PROJECTION);
-   glLoadIdentity ();
-   gluPerspective(30, (GLfloat) w/(GLfloat) h, 1.0, 200.0);
-   glMatrixMode (GL_MODELVIEW);
-
+	if (h==0){
+		h=1;	
+	}
+	glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity ();
+	gluPerspective(30, (GLfloat) w/(GLfloat) h, 1.0, 200.0);
+	glMatrixMode (GL_MODELVIEW);
 }
 
 void init_surface() {
 	int x = 10;
-	int y = 10;
-	int z = 0;
+	int y = 0;
+	int z = 10;
 
 	for (int f = 0; f < 21; f++) {
 		for (int c = 0; c < 21; c++) {
 			ctlpoints[f][c][0] = x - c;
-			ctlpoints[f][c][1] = z;
-			ctlpoints[f][c][2] = y; 
+			ctlpoints[f][c][1] = y;
+			ctlpoints[f][c][2] = z; 
 		}
-		y -= 1;
+		z -= 1;
 	}		
 }
 
 void init(){
-
-
-
    glEnable(GL_LIGHTING);
    glEnable(GL_LIGHT0);
    glEnable(GL_DEPTH_TEST);
@@ -130,21 +131,136 @@ void init(){
    theNurb = gluNewNurbsRenderer();
    gluNurbsProperty(theNurb, GLU_SAMPLING_TOLERANCE, 15.0);
    gluNurbsProperty(theNurb, GLU_DISPLAY_MODE, GLU_FILL);
-
-	
-
 }
 
 void Keyboard(unsigned char key, int x, int y){
   switch (key){
-	case 'r':
-		//glDisable(GLenum);
-	case 'p':
-		//glEnable(GLenum);
-	case 27:             
+	case '1':
+		activarOla1 = true;
+		activarOla2 = false;
+		break;
+	case '2':
+		activarOla1 = false;
+		activarOla2 = true;
+		break;
+	case 97:	//97 en ASCII es: a.
+		if (activarOla1) {
+			L1 -= 0.1;
+		} else if (activarOla2) {
+			L2 -= 0.1;
+		} else {
+			break;
+		}
+	case 122:	//122 en ASCII es: z.
+		if (activarOla1) {
+			L1 += 0.1;
+		} else if (activarOla2) {
+			L2 += 0.1;
+		} else {
+			break;
+		}
+	case 115:	//115 en ASCII es: s.
+		if (activarOla1) {
+			A1 -= 0.1;
+		} else if (activarOla2) {
+			A2 -= 0.1;
+		} else {
+			break;
+		}
+	case 120:	//120 en ASCII es: x.
+		if (activarOla1) {
+			A1 += 0.1;
+		} else if (activarOla2) {
+			A2 += 0.1;
+		} else {
+			break;
+		}
+	case 100:	//100 en ASCII es: d.
+		if (activarOla1) {
+			S1 -= 0.1;
+		} else if (activarOla2) {
+			S2 -= 0.1;
+		} else {
+			break;
+		}
+	case 99:	//99 en ASCII es: c.
+		if (activarOla1) {
+			S1 += 0.1;
+		} else if (activarOla2) {
+			S2 += 0.1;
+		} else {
+			break;
+		}
+	case 102:	//102 en ASCII es: f.
+		if (activarOla1) {
+			D1.x -= 0.1;
+		} else if (activarOla2) {
+			D2.x -= 0.1;
+		} else {
+			break;
+		}
+	case 118:	//118 en ASCII es: v.
+		if (activarOla1) {
+			D1.x += 0.1;
+		} else if (activarOla2) {
+			D2.x += 0.1;
+		} else {
+			break;
+		}
+	case 103:	//103 en ASCII es: g.
+		if (activarOla1) {
+			D1.y -= 0.1;
+		} else if (activarOla2) {
+			D2.y -= 0.1;
+		} else {
+			break;
+		}
+	case 98:	//98 en ASCII es: b.
+		if (activarOla1) {
+			D1.y += 0.1;
+		} else if (activarOla2) {
+			D2.y += 0.1;
+		} else {
+			break;
+		}
+	case 114: //114 en ASCII es: r.
+		pausar = false;  
+		break;
+	case 112: //112 en ASCII es: p.
+		pausar = true;
+		break;
+	case 27:  //27 en ASCII es: ESC.        
 		exit (0);
 		break;	
   }
+}
+
+void animacion(int value) {
+	if (!pausar){
+		t += 0.1;
+		w1 = (2 * pi) / L1;	//Onda 1
+		w2 = (2 * pi) / L2;	//Onda 2
+		signo1 = S1 * w1;	//Valor de signo extraño 1
+		signo2 = S2 * w2;	//Valor de signo extraño 2
+		normal1.x = (1 / (sqrt((D1.x)*(D1.x) + (D1.y)*(D1.y)))) * D1.x;	// Normalizar componente x del vector D1
+		normal1.y = (1 / (sqrt((D1.x)*(D1.x) + (D1.y)*(D1.y)))) * D1.y; // Normalizar componente y del vector D1
+		normal2.x = (1 / (sqrt((D2.x)*(D2.x) + (D2.y)*(D2.y)))) * D2.x; // Normalizar componente x del vector D2
+		normal2.y = (1 / (sqrt((D2.x)*(D2.x) + (D2.y)*(D2.y)))) * D2.y; // Normalizar componente y del vector D2
+		for (int f = 0; f < 10; f++) {
+			for (int c = 0; c < 21; c++) {
+				escalar1	= (normal1.x * ctlpoints[f][c][0]) + (normal1.y * ctlpoints[f][c][2]); 
+				ctlpoints[f][c][1] = A1 * sin(escalar1 + t * signo1);
+			}
+		}		
+		for (int f = 11; f < 21; f++) {
+			for (int c = 0; c < 21; c++) {
+				escalar2	= (normal2.x * ctlpoints[f][c][0]) + (normal2.y * ctlpoints[f][c][2]); 
+				ctlpoints[f][c][1] = A2 * sin(escalar2 + t * signo2);
+			}
+		}		
+	}
+	glutTimerFunc(1000,animacion,1);
+	glutPostRedisplay();	
 }
 
 void render(){
@@ -157,9 +273,7 @@ void render(){
 	glLoadIdentity ();                       
 	gluLookAt (25.0, 12.0, 4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	
-
 	// Luz y material
-
 	GLfloat mat_diffuse[] = { 0.6, 0.6, 0.9, 1.0 };
 	GLfloat mat_specular[] = { 0.8, 0.8, 1.0, 1.0 };
 	GLfloat mat_shininess[] = { 60.0 };
@@ -168,7 +282,6 @@ void render(){
 	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
 	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 	
-
     GLfloat light_ambient[] = { 0.0, 0.0, 0.2, 1.0 };
 	GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
 	GLfloat light_specular[] = { 0.6, 0.6, 0.6, 1.0 };
@@ -179,8 +292,6 @@ void render(){
 	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);   
 
-
-	
 	// Render Grid 
 /*	glDisable(GL_LIGHTING);
 	glLineWidth(1.0);
@@ -208,30 +319,23 @@ void render(){
 	glEnable(GL_LIGHTING);*/
 	// Fin Grid
 	
-
-
 	//Suaviza las lineas
 	glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable( GL_LINE_SMOOTH );	
-
 	
 	glPushMatrix();
-
-	gluBeginSurface(theNurb);
-    
-	gluNurbsSurface(theNurb, 
-		25,knotsSurf,25,knotsSurf,
-				   21 * 3, 3, &ctlpoints[0][0][0], 
-                   4, 4, GL_MAP2_VERTEX_3);
-
-	gluEndSurface(theNurb);
-	
+		gluBeginSurface(theNurb);
+		gluNurbsSurface(theNurb, 
+			25,knotsSurf,25,knotsSurf,
+					   21 * 3, 3, &ctlpoints[0][0][0], 
+					   4, 4, GL_MAP2_VERTEX_3);
+		gluEndSurface(theNurb);
 	glPopMatrix();
 	
 	
 	/* Muestra los puntos de control */
 	
-		int i,j;
+	/*	int i,j;
 		glPointSize(5.0);
 		glDisable(GL_LIGHTING);
 		glColor3f(1.0, 1.0, 0.0);
@@ -243,37 +347,17 @@ void render(){
 		}
 		glEnd();
 		glEnable(GL_LIGHTING);
-	
+	*/
 		
-
 	glDisable(GL_BLEND);
 	glDisable(GL_LINE_SMOOTH);
 
+	//Pausamos la animación de las olas.
+	if (!pausar) {
+		glutTimerFunc(1000,animacion,1);
+	}
+
 	glutSwapBuffers();
-}
-
-void animacion(int value) {
-	t += 0.1;
-	for (int f = 0; f < 21; f++) {
-		for (int c = 0; c < 21; c++) {
-			ctlpoints[f][c][1] = -3*sin(t);
-			ctlpoints[f][c][1] = 3*sin(t);
-			ctlpoints[f][c][1] = -3*sin(t); 
-		}
-	}		
-
-/*	for (int f = 11; f < 21; f++) {
-		for (int c = 0; c < 21; c++) {
-			ctlpoints[f][c][1] = -2*sin(t);
-			ctlpoints[f][c][1] = 2*sin(t);
-			ctlpoints[f][c][1] = -2*sin(t); 
-		}
-	}		*/
-
-
-	glutTimerFunc(10,animacion,1);
-    glutPostRedisplay();
-	
 }
 
 int main (int argc, char** argv) {
@@ -291,9 +375,6 @@ int main (int argc, char** argv) {
 	glutReshapeFunc(changeViewport);
 	glutDisplayFunc(render);
 	glutKeyboardFunc(Keyboard);
-	
-	//glutTimerFunc(10,animacion,1);
-
 
 	GLenum err = glewInit();
 	if (GLEW_OK != err) {
@@ -303,5 +384,4 @@ int main (int argc, char** argv) {
 	
 	glutMainLoop();
 	return 0;
-
 }
