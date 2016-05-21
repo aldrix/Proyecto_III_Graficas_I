7a14fa4136b81618d282bc16208ee0dbe89fe332
aldrix <aldrix_marfil@hotmail.com>
2016-05-21 14:22:09 -0430

    Funciona el pausar al presionar la tecla p y quitar la pausa con r.

--- PlantillaOpenGL01/main.cpp
+++ PlantillaOpenGL01/main.cpp
@@ -42,10 +42,11 @@ float S2	   = 0.0f;			//Velocidad de la ola 2
 Vector2D D2 = {0};				//Vector que determina la dirección de la ola 2
 
 float t = 0.0f;
+bool  pausar = true;
 
 
 void ejesCoordenada() {
-	
+
 	glLineWidth(2.5);
 	glBegin(GL_LINES);
 		glColor3f(1.0,0.0,0.0);
@@ -74,51 +75,41 @@ void ejesCoordenada() {
 
 					glVertex2f(0.2,i);
 					glVertex2f(-0.2,i);
-
 				}
 			}
 		}
-		
 	glEnd();
-
 	glLineWidth(1.0);
 }
 
 void changeViewport(int w, int h) {
-	
 	float aspectratio;
 
-	if (h==0)
-		h=1;
-
+	if (h==0) h=1;
 	
    glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
    glMatrixMode (GL_PROJECTION);
    glLoadIdentity ();
    gluPerspective(30, (GLfloat) w/(GLfloat) h, 1.0, 200.0);
    glMatrixMode (GL_MODELVIEW);
-
 }
 
 void init_surface() {
 	int x = 10;
-	int y = 10;
-	int z = 0;
+	int y = 0;
+	int z = 10;
 
 	for (int f = 0; f < 21; f++) {
 		for (int c = 0; c < 21; c++) {
 			ctlpoints[f][c][0] = x - c;
-			ctlpoints[f][c][1] = z;
-			ctlpoints[f][c][2] = y; 
+			ctlpoints[f][c][1] = y;
+			ctlpoints[f][c][2] = z; 
 		}
-		y -= 1;
+		z -= 1;
 	}		
 }
 
 void init(){
-
-
-
    glEnable(GL_LIGHTING);
    glEnable(GL_LIGHT0);
    glEnable(GL_DEPTH_TEST);
@@ -130,36 +121,57 @@ void init(){
    theNurb = gluNewNurbsRenderer();
    gluNurbsProperty(theNurb, GLU_SAMPLING_TOLERANCE, 15.0);
    gluNurbsProperty(theNurb, GLU_DISPLAY_MODE, GLU_FILL);
-
-	
-
 }
 
 void Keyboard(unsigned char key, int x, int y){
   switch (key){
-	case 'r':
-		//glDisable(GLenum);
-	case 'p':
-		//glEnable(GLenum);
-	case 27:             
+	case 114: //114 en ASCII es: r.
+		pausar = false;  
+	break;
+	case 112: //112 en ASCII es: p.
+		pausar = true;
+	break;
+	case 27:  //27 en ASCII es: ESC.        
 		exit (0);
 		break;	
   }
+  glutPostRedisplay();
+}
+
+void animacion(int value) {
+	if (!pausar) { //Si no esta en pausa se mustra la animacion.
+		t += 0.1;
+		for (int f = 0; f < 21; f++) {
+			for (int c = 0; c < 21; c++) {
+				ctlpoints[f][c][1] = -3*sin(t);
+				ctlpoints[f][c][1] = 3*sin(t);
+				ctlpoints[f][c][1] = -3*sin(t); 
+			}
+		}
+		glutPostRedisplay();
+	}
+
+/*	for (int f = 11; f < 21; f++) {
+		for (int c = 0; c < 21; c++) {
+			ctlpoints[f][c][1] = -2*sin(t);
+			ctlpoints[f][c][1] = 2*sin(t);
+			ctlpoints[f][c][1] = -2*sin(t); 
+		}
+	}		*/
 }
 
 void render(){
+	
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	GLfloat zExtent, xExtent, xLocal, zLocal;
-    int loopX, loopZ;
+	int loopX, loopZ;
 
 	glLoadIdentity ();                       
 	gluLookAt (25.0, 12.0, 4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	
 
 	// Luz y material
-
 	GLfloat mat_diffuse[] = { 0.6, 0.6, 0.9, 1.0 };
 	GLfloat mat_specular[] = { 0.8, 0.8, 1.0, 1.0 };
 	GLfloat mat_shininess[] = { 60.0 };
@@ -168,8 +180,7 @@ void render(){
 	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
 	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 	
-
-    GLfloat light_ambient[] = { 0.0, 0.0, 0.2, 1.0 };
+	GLfloat light_ambient[] = { 0.0, 0.0, 0.2, 1.0 };
 	GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
 	GLfloat light_specular[] = { 0.6, 0.6, 0.6, 1.0 };
 	GLfloat light_position[] = { -10.0, 5.0, 0.0, 1.0 };
@@ -179,122 +190,86 @@ void render(){
 	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);   
 
-
-	
 	// Render Grid 
-/*	glDisable(GL_LIGHTING);
-	glLineWidth(1.0);
-	glPushMatrix();
-	glRotatef(90,1.0,0.0,0.0);
-    glColor3f( 0.0, 0.7, 0.7 );
-    glBegin( GL_LINES );
-    zExtent = DEF_floorGridScale * DEF_floorGridZSteps;
-    for(loopX = -DEF_floorGridXSteps; loopX <= DEF_floorGridXSteps; loopX++ )
-	{
-	xLocal = DEF_floorGridScale * loopX;
-	glVertex3f( xLocal, -zExtent, 0.0 );
-	glVertex3f( xLocal, zExtent,  0.0 );
+	/*	glDisable(GL_LIGHTING);
+		glLineWidth(1.0);
+		glPushMatrix();
+		glRotatef(90,1.0,0.0,0.0);
+		glColor3f( 0.0, 0.7, 0.7 );
+		glBegin( GL_LINES );
+		zExtent = DEF_floorGridScale * DEF_floorGridZSteps;
+		for(loopX = -DEF_floorGridXSteps; loopX <= DEF_floorGridXSteps; loopX++ )
+		{
+		xLocal = DEF_floorGridScale * loopX;
+		glVertex3f( xLocal, -zExtent, 0.0 );
+		glVertex3f( xLocal, zExtent,  0.0 );
+		}
+		xExtent = DEF_floorGridScale * DEF_floorGridXSteps;
+		for(loopZ = -DEF_floorGridZSteps; loopZ <= DEF_floorGridZSteps; loopZ++ )
+		{
+		zLocal = DEF_floorGridScale * loopZ;
+		glVertex3f( -xExtent, zLocal, 0.0 );
+		glVertex3f(  xExtent, zLocal, 0.0 );
 	}
-    xExtent = DEF_floorGridScale * DEF_floorGridXSteps;
-    for(loopZ = -DEF_floorGridZSteps; loopZ <= DEF_floorGridZSteps; loopZ++ )
-	{
-	zLocal = DEF_floorGridScale * loopZ;
-	glVertex3f( -xExtent, zLocal, 0.0 );
-	glVertex3f(  xExtent, zLocal, 0.0 );
-	}
-    glEnd();
+	glEnd();
 	ejesCoordenada();
-    glPopMatrix();
+	glPopMatrix();
 	glEnable(GL_LIGHTING);*/
 	// Fin Grid
-	
-
 
 	//Suaviza las lineas
 	glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable( GL_LINE_SMOOTH );	
 
-	
 	glPushMatrix();
-
 	gluBeginSurface(theNurb);
-    
+   
 	gluNurbsSurface(theNurb, 
-		25,knotsSurf,25,knotsSurf,
-				   21 * 3, 3, &ctlpoints[0][0][0], 
-                   4, 4, GL_MAP2_VERTEX_3);
+					25,knotsSurf,25,knotsSurf,
+					21 * 3, 3, &ctlpoints[0][0][0], 
+					4, 4, GL_MAP2_VERTEX_3);
 
 	gluEndSurface(theNurb);
-	
 	glPopMatrix();
-	
-	
-	/* Muestra los puntos de control */
-	
-		int i,j;
-		glPointSize(5.0);
-		glDisable(GL_LIGHTING);
-		glColor3f(1.0, 1.0, 0.0);
-		glBegin(GL_POINTS);
-		for (i = 0; i < 21; i++) {
-			for (j = 0; j < 21; j++) {
-	            glVertex3f(ctlpoints[i][j][0], 	ctlpoints[i][j][1], ctlpoints[i][j][2]);
-			}
-		}
-		glEnd();
-		glEnable(GL_LIGHTING);
-	
-		
 
+	/* Muestra los puntos de control */
+	/*glPointSize(5.0);
+	glDisable(GL_LIGHTING);
+	glColor3f(1.0, 1.0, 0.0);
+	glBegin(GL_POINTS);
+	for (int i = 0; i < 21; i++) {
+		for (int j = 0; j < 21; j++) {
+			glVertex3f(ctlpoints[i][j][0], 	ctlpoints[i][j][1], ctlpoints[i][j][2]);
+		}
+	}*/
+
+	glEnd();
+	glEnable(GL_LIGHTING);
 	glDisable(GL_BLEND);
 	glDisable(GL_LINE_SMOOTH);
+
+	//Pausamos la animacion de las olas.
+	if (!pausar) {
+		glutTimerFunc(10,animacion,1);
+	}
 
 	glutSwapBuffers();
 }
 
-void animacion(int value) {
-	t += 0.1;
-	for (int f = 0; f < 21; f++) {
-		for (int c = 0; c < 21; c++) {
-			ctlpoints[f][c][1] = -3*sin(t);
-			ctlpoints[f][c][1] = 3*sin(t);
-			ctlpoints[f][c][1] = -3*sin(t); 
-		}
-	}		
-
-/*	for (int f = 11; f < 21; f++) {
-		for (int c = 0; c < 21; c++) {
-			ctlpoints[f][c][1] = -2*sin(t);
-			ctlpoints[f][c][1] = 2*sin(t);
-			ctlpoints[f][c][1] = -2*sin(t); 
-		}
-	}		*/
-
-
-	glutTimerFunc(10,animacion,1);
-    glutPostRedisplay();
-	
-}
 
 int main (int argc, char** argv) {
 
 	glutInit(&argc, argv);
-
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-
 	glutInitWindowSize(960,540);
-
 	glutCreateWindow("Nurbs Proyecto - Ola");
 
 	init ();
 
 	glutReshapeFunc(changeViewport);
-	glutDisplayFunc(render);
 	glutKeyboardFunc(Keyboard);
+	glutDisplayFunc(render);
 	
-	//glutTimerFunc(10,animacion,1);
-
-
 	GLenum err = glewInit();
 	if (GLEW_OK != err) {
 		fprintf(stderr, "GLEW error");
@@ -303,5 +278,4 @@ int main (int argc, char** argv) {
 	
 	glutMainLoop();
 	return 0;
-
 }
